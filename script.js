09dae05cf746da6feb3f2cac73a0b9bcb3282f9d
lusuga404 <lusuga404@gmail.com>
2025-08-07 16:46:53 -0500

    se crea repositorio de ejercicio funciones factura electrica

---
+++ script.js
@@ -0,0 +1,14 @@
+function consumoMensualkilovatios(){
+let cantidadKilovatios = Number (prompt("ingrese la cantidad de kilovatios consumidos en el Mes:"))
+console.log("la cantidad de kilovatios es:" + cantidadKilovatios)
+return cantidadKilovatios
+}
+consumoMensualkilovatios()
+
+
+function tarifaBaseKilovatio(){
+    let tarifaKilovatio = Number (prompt("ingrese la tarifa por kilovatio:"))
+    console.log("la tarifa de kilovatio es:" + tarifaKilovatio)
+    return tarifaKilovatio
+}
+tarifaBaseKilovatio()
